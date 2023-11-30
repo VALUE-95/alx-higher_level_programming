c8e0fbee72f67fe5d01c9a0076e111f689cde1b2
value <vochuba95@gmail.com>
2023-11-30 05:40:05 -0800

    here again

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-chr = ord("#") // ord("#")
-chr and __import__('os').write(1, b"#pythoniscool\n")
+chr = ord("#") // ord("#"); chr
+and __import__('os').write(1, b"#pythoniscool\n")
