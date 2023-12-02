4f6d49770082412d13fde3931cf049f364cb69ae
value <vochuba95@gmail.com>
2023-12-02 00:56:03 -0800

    Adjustments

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
