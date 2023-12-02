db3375af2424a73bb5c53a9039a9779b8b40f15c
value <vochuba95@gmail.com>
2023-12-02 02:06:46 -0800

    Adjustments

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # If a tuple is smaller than 2, use 0 for each missing integer
-    a = (0, 0) if len(tuple_a) < 2 else tuple_a[:2]
-    b = (0, 0) if len(tuple_b) < 2 else tuple_b[:2]
+    a_first = tuple_a[0] if len(tuple_a) >= 1 else 0
+    a_second = tuple_a[1] if len(tuple_a) >= 2 else 0
+    b_first = tuple_b[0] if len(tuple_b) >= 1 else 0
+    b_second = tuple_b[1] if len(tuple_b) >= 2 else 0
 
-    return (a[0] + b[0], a[1] + b[1])
+    sum_first = a_first + b_first
+    sum_second = a_second + b_second
+
+    return (sum_first, sum_second)
