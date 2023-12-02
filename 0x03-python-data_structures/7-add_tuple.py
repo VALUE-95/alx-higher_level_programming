cb374eccc78517943e3b224629d71debb1297f2e
value <vochuba95@gmail.com>
2023-12-02 01:23:08 -0800

    Adjustments

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
-    result = (a[0] + b[0], a[1] + b[1])
-    return result
+    # If a tuple is smaller than 2, use 0 for each missing integer
+    a = (0, 0) if len(tuple_a) < 2 else tuple_a[:2]
+    b = (0, 0) if len(tuple_b) < 2 else tuple_b[:2]
+
+    return (a[0] + b[0], a[1] + b[1])
