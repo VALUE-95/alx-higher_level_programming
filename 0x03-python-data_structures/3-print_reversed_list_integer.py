a9bc648247527c8aa58ee2f5e803d50efe2b14e3
value <vochuba95@gmail.com>
2023-12-02 01:05:31 -0800

    Adjustments

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for integer in reversed(my_list):
-        print("{:d}".format(integer))
+    for number in reversed(my_list):
+        print("{:d}".format(number))
