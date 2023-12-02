facff6e17b83f89a15de5c7dfd11b1ddaac03abb
value <vochuba95@gmail.com>
2023-12-02 01:35:11 -0800

    Adjustments

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        for i in range(idx, len(my_list) - 1):
-            my_list[i] = my_list[i + 1]
-        my_list.pop()
-    return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    return my_list[:idx] + my_list[idx + 1:]
