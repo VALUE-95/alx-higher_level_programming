1d0527c01dad6863b5b401ffa089b6b44dc0004d
value <vochuba95@gmail.com>
2023-12-02 02:11:42 -0800

    Updated

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_list = my_list[:idx] + my_list[idx + 1:]
+    new_list = [value for i, value in enumerate(my_list) if i != idx]
     return new_list
