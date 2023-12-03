079e05f0dec12d1bd4f47900c71d6f81e341aa3a
OCHUBA Victor Ogechukwu <vochuba95@gmail.com>
2023-12-03 22:45:05 +0100

    Done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print("Matrix is empty")
-    else:
-        for row in matrix:
-            for number in row:
-                print("{:d}".format(number), end=" ")
-
-            print()
+    for frow in matrix:
+        for fcol in frow:
+            print("{:d}".format(fcol), end=" " if fcol != frow[-1] else "")
+        print()
