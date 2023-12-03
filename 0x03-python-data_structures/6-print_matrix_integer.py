67c09d4c425e8837490d3cec43d136882b103aa0
value <vochuba95@gmail.com>
2023-12-03 13:50:11 -0800

    Adjustments

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for frow in matrix:
         for fcol in frow:
             print("{:d}".format(fcol), end=" " if fcol != frow[-1] else "")
+
         print()
