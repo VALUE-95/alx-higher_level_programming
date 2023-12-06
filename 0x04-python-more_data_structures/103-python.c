6d393ebb210a25ac0634e080e8029ced3b6b1066
value <vochuba95@gmail.com>
2023-12-06 09:20:57 -0800

    changing times

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,15 +1,7 @@
 #include <Python.h>
-#include <stdio.h>
 
-/**
- * print_python_bytes - Print information about Python bytes objects
- * @p: Python object (bytes)
- */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i;
-	char *str;
-
 	printf("[.] bytes object info\n");
 
 	if (!PyBytes_Check(p))
@@ -18,49 +10,56 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	size = PyBytes_GET_SIZE(p);
-	str = PyBytes_AS_STRING(p);
+	Py_ssize_t size = PyBytes_Size(p);
+	char *str = PyBytes_AsString(p);
 
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", str);
-	printf("  first 10 bytes:");
 
-	for (i = 0; i < size && i < 10; ++i)
-		printf(" %02x", (unsigned char)str[i]);
-
+	printf("  first 10 bytes: ");
+	for (Py_ssize_t i = 0; i < size && i < 10; ++i)
+	{
+		printf("%02x ", (unsigned char)str[i]);
+	}
 	printf("\n");
 }
 
-/**
- * print_python_list - Print information about Python lists
- * @p: Python object (list)
- */
 void print_python_list(PyObject *p)
 {
-	PyListObject *list_obj;
-	Py_ssize_t size, i;
-	PyObject *item;
-
 	printf("[*] Python list info\n");
 
-	if (!PyList_Check(p))
-	{
-		printf("[ERROR] Invalid List Object\n");
-		return;
-	}
-
-	list_obj = (PyListObject *)p;
-	size = PyList_GET_SIZE(p);
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
 
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list_obj->allocated);
+	printf("[*] Allocated = %ld\n", allocated);
 
-	for (i = 0; i < size; ++i)
+	for (Py_ssize_t i = 0; i < size; ++i)
 	{
-		item = PyList_GET_ITEM(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		PyObject *element = PyList_GetItem(p, i);
+		printf("Element %ld: ", i);
 
-		if (PyBytes_Check(item))
-			print_python_bytes(item);
+		if (PyBytes_Check(element))
+		{
+			print_python_bytes(element);
+		} else if (PyLong_Check(element))
+		{
+			printf("int\n");
+		} else if (PyFloat_Check(element))
+		{
+			printf("float\n");
+		} else if (PyTuple_Check(element))
+		{
+			printf("tuple\n");
+		} else if (PyList_Check(element))
+		{
+			printf("list\n");
+		} else if (PyUnicode_Check(element))
+		{
+			printf("str\n");
+		} else
+		{
+			printf("unknown\n");
+		}
 	}
 }
