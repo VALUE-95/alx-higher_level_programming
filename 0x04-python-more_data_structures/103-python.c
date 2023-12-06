61d375bd1781ac133827483bae00cce6b0026a61
value <vochuba95@gmail.com>
2023-12-06 15:09:36 -0800

    Tried

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -10,20 +10,20 @@
 
 void print_python_list(PyObject *p)
 {
-        Py_ssize_t size, i;
-        PyListObject *list = (PyListObject *p)p;
+	Py_ssize_t size, i;
+	PyListObject *list = (PyListObject *p)p;
 
-        printf("[*] Python list info\n");
-        size = Py_SIZE(p);
-        printf("[*] Size of the Python List = %zd\n", size);
-        printf("[*] Allocated = %zd\n", list->allocated);
+	printf("[*] Python list info\n");
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", list->allocated);
 
-        for (i = 0; i < size; ++i)
-        {
-                printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-                if (PyBytes_Check(PyList_GetItem(p, i)))
-                        print_python_bytes(PyList_GetItem(p, i));
-        }
+	for (i = 0; i < size; ++i)
+	{
+		printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+		if (PyBytes_Check(PyList_GetItem(p, i)))
+			print_python_bytes(PyList_GetItem(p, i));
+	}
 }
 
 
@@ -36,26 +36,26 @@ void print_python_list(PyObject *p)
 
 void print_python_bytes(PyObject *p)
 {
-        Py_ssize_t size, i;
-        unsigned char *str;
+	Py_ssize_t size, i;
+	unsigned char *str;
 
-        printf("[.] bytes object info\n");
+	printf("[.] bytes object info\n");
 
-        if (!PyBytes_Check(p))
-        {
-                printf(" [ERROR] Invalid Bytes Object\n");
-                return;
-        }
+	if (!PyBytes_Check(p))
+	{
+		printf(" [ERROR] Invalid Bytes Object\n");
+		return;
+	}
 
-        size = PyBytes_GET_SIZE(p);
-        str = (unsigned char *)PyBytes_AS_STRING(p);
+	size = PyBytes_GET_SIZE(p);
+	str = (unsigned char *)PyBytes_AS_STRING(p);
 
-        printf(" size: %zd\n", size);
-        printf(" trying string: %s\n", str);
-        printf(" first 10 bytes:");
+	printf(" size: %zd\n", size);
+	printf(" trying string: %s\n", str);
+	printf(" first 10 bytes:");
 
-        for (i - 0; i < size && i < 10; i++)
-                printf(" %02x", str[i]);
+	for (i - 0; i < size && i < 10; i++)
+		printf(" %02x", str[i]);
 
-        printf("\n");
+	printf("\n");
 }
