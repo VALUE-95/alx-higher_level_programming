2e9e1a943be4e327511509a422c6e0e6c356189b
value <vochuba95@gmail.com>
2024-01-04 10:40:37 -0800

    test

--- 0x00-python-hello_world/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-
-    for char in str:
-        if ord("a") <= ord(char) <= ord("z"):
-            char = chr(ord(char) - 32)
-        print("{:s}".format(char), end="")
-        print()
