592fbe7d00da9f38de40997b95a3e1cd80d9ae53
value <vochuba95@gmail.com>
2024-01-09 05:36:03 -0800

    test again

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
+
 """Add all arguments to a Python list and save them to a file."""
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+import argparse
+import json
+
+
+def load_items(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+def save_items(items, file_path):
+    with open(file_path, 'w') as file:
+        json.dump(items, file)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Add\
+            items to a Python list and save to a file.")
+    parser.add_argument('items', nargs='*', help='Items to add to the list')
+    args = parser.parse_args()
+
+    file_path = "add_item.json"
+
+    items = load_items(file_path)
+    items.extend(args.items)
+    save_items(items, file_path)
+
 
 if __name__ == "__main__":
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    main()
